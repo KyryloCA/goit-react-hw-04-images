8fee8571b47e3fff9cd1e5455a4170c93e6955d3
KyryloCA <3dprofix@gmail.com>
2023-04-19 21:54:56 -0400

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import React, { useState } from 'react'
+
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import css from './App.module.css';
+
+const App = () => {
+const [searchWord, setSearchWord] = useState('')
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    setSearchWord(e.target.searchWord.value.trim())
+    
+    // this.setState({searchWord:e.target.searchWord.value.trim()})
+  }
+    return (
+      <div className={css.App}>
+  <Searchbar onSubmit={onSubmit} searchWord={searchWord}/>
+     <ImageGallery searchWord={searchWord}/>
     </div>
-  );
-};
+    )
+  
+}
+
+export {App};
+
+
