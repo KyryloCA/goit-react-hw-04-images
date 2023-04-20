625d9ec91175bc8073b0c93f978972025b5e75cc
KyryloCA <3dprofix@gmail.com>
2023-04-20 13:57:26 -0400

    fixed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,92 +2,81 @@ import {api} from "../../api/Api"
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
 
-import React, { useState, useEffect, } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem"
 import Loader from '../Loader/Loader'; 
 import css from './ImageGallery.module.css';
 
- const ImageGallery = (props)=>{
+const ImageGallery = (props)=>{
+  const [itemsArray, setItemsArray] = useState([])
+  const [pageCounter, setPageCounter] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [modalContent, setModalContent] = useState(null)
+  const [modalState, setModalState] = useState(false)
 
- 
-
-const [itemsArray, setItemsArray] = useState([])
-const [pageCounter, setPageCounter] = useState(1)
-const [loading, setLoading] = useState(false)
-const [modalContent, setModalContent] = useState(null)
-const [modalState, setModalState] = useState(false)
-
-
-const addPage = () => {
-setPageCounter(prevState=>prevState + 1)
-}
-
-
-const pushItemsArray=(arr)=>{
-  setItemsArray(prevState=>[...prevState,...arr])
- }
-
-const displayModal = ({ link, desc }) => {
-  setModalContent({ link, desc })
-  setModalState(true)
-}
-
-const closeModal = () => {
-  setModalContent(null)
-  setModalState(false)
- }
-
-
-
-
-
-useEffect(() => {
-  const handleSearch = (searchWord,page)=>{
+  const prevSearchWordRef = useRef();
   
-    setLoading(true)
+  const addPage = () => {
+    setPageCounter(prevState=>prevState + 1)
+  }
+
+  const pushItemsArray=(arr)=>{
+    setItemsArray(prevState=>[...prevState,...arr])
+  }
+
+  const displayModal = ({ link, desc }) => {
+    setModalContent({ link, desc })
+    setModalState(true)
+  }
+
+  const closeModal = () => {
+    setModalContent(null)
+    setModalState(false)
+  }
+
+  useEffect(() => {
+    const handleSearch = (searchWord,page)=>{
+      setLoading(true)
       api(searchWord, page)
-      .then((arr) => {
-        pushItemsArray(arr);
-        setLoading(false)
-            })
-      .catch(() => {
-        setLoading(false)
-            });
-  }
-  if (props.searchWord!==''){setItemsArray([]);
-    setPageCounter(1);
-    handleSearch(props.searchWord, 1);}
-  
-}, [ props.searchWord]);
+        .then((arr) => {
+          pushItemsArray(arr);
+          setLoading(false)
+        })
+        .catch(() => {
+          setLoading(false)
+        });
+    }
+    if (props.searchWord !== '') {
+      setItemsArray([]);
+      setPageCounter(1);
+      prevSearchWordRef.current = props.searchWord;
+      handleSearch(props.searchWord, 1);
+    }
+  }, [props.searchWord]);
 
-useEffect(() => {
-  const handleSearch = (searchWord,page)=>{
-  
-    setLoading(true)
+  useEffect(() => {
+    const handleSearch = (searchWord,page)=>{
+      setLoading(true)
       api(searchWord, page)
-      .then((arr) => {
-        pushItemsArray(arr);
-        setLoading(false)
-            })
-      .catch(() => {
-        setLoading(false)
-            });
-  }
-  if (pageCounter > 1) {
-    handleSearch(props.searchWord, pageCounter);
-  }
-  
-}, [pageCounter, props.searchWord]);
+        .then((arr) => {
+          pushItemsArray(arr);
+          setLoading(false)
+        })
+        .catch(() => {
+          setLoading(false)
+        });
+    }
+    if (pageCounter > 1) {
+      handleSearch(prevSearchWordRef.current, pageCounter);
+    }
+  }, [pageCounter]);
 
-
-    
-    return (
-      <>
+  return (
+    <>
       <ul className={css.ImageGallery}>
         {itemsArray.length>0 && itemsArray.map((el)=>(
           <ImageGalleryItem key={el.id} src={el.webformatURL} desc={el.id} largeImageURL={el.largeImageURL} displayModal={displayModal}/>
         ))}
-        
       </ul>
       {loading && <Loader />}
       {props.searchWord && itemsArray.length>0 && <Button addPage={addPage} />}
@@ -99,10 +88,8 @@ useEffect(() => {
           closeModal={closeModal}
         />
       )}
-      </>
-    )
-  
+    </>
+  )
 }
-
 
 export default ImageGallery
